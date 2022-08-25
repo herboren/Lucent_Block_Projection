7a408c986f23a0e1f99d78e57256f90bf451936d
LowlandVik <herboren@users.noreply.github.com>
2022-08-25 15:02:03 -0400

    Working Code
    Instance being used to pass data between Session and UseObject. Working as intended.

--- LucentBlockProjection.cs
+++ LucentBlockProjection.cs
@@ -1,4 +1,4 @@
-﻿using Sandbox.Game;
+﻿ using Sandbox.Game;
 using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
 using System.Collections.Generic;
@@ -8,31 +8,26 @@ using VRageMath;
 using VRage.Game.ModAPI;
 using Sandbox.Game.EntityComponents;
 using VRage.Utils;
+using SpaceEngineers.Game.ModAPI;
+using VRage.Game;
+using System.IO;
+using VRage;
 
 namespace TestEnv
 {
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
     public class LucentBlockProjection : MySessionComponentBase
     {
+        public static LucentBlockProjection Instance;
+        
         BoundingSphereD sphere;
         GetPlayerCameraSphereD boundingSphere;
-
-        // Power Distribution
-        private MyResourceSourceComponent r_PowerSource = null;
-        private MyResourceSinkComponent r_PowerSink = null;
-        private MyResourceDistributorComponent r_PowerDistributor = null;
-
         public IMyCubeGrid Grid;
-
+        public bool IsPressed { get; set; }
         /// <summary>
         /// Get list of entities from player camera sphere
         /// </summary>
         private readonly List<MyEntity> _entList = new List<MyEntity>();
-
-        /// <summary>
-        /// Get list of entity IDs found within player camera sphere.
-        /// </summary>
-        private readonly List<long> _entityIds = new List<long>();
 
         /// <summary>
         /// Keep track of ticks, helps to reduce # of updates
@@ -41,11 +36,13 @@ namespace TestEnv
 
         public override void UpdateAfterSimulation()
         {
-            Ticks++;
+            Instance = this;
 
+            Ticks++;
+                        
             if (Ticks % 120 == 0)
             {
-                new GetPlayerCameraSphereD(MyAPIGateway.Session.Camera, 100, out sphere);
+                new GetPlayerCameraSphereD(MyAPIGateway.Session.Camera, 10, out sphere);
                 MyGamePruningStructure.GetAllTopMostEntitiesInSphere(ref sphere, _entList);
 
                 foreach (MyEntity entity in _entList)
@@ -58,33 +55,40 @@ namespace TestEnv
                     }
                 }
 
-                r_PowerSource = new MyResourceSourceComponent();
-                r_PowerSink = new MyResourceSinkComponent();
-                
                 // Lets find the appropriate blocks.
                 foreach (MyCubeBlock cube in Grid.GetFatBlocks<IMyCubeBlock>())
                 {
-                    if (cube is IMyConveyorTube || cube is IMyConveyorSorter || cube is IMyConveyor)
+                    if (cube != null && cube is IMyConveyorTube   ||
+                                        cube is IMyConveyorSorter ||
+                                        cube is IMyConveyor       ||
+                                        cube is IMyRefinery       ||
+                                        cube is IMyAssembler      || 
+                                        cube is IMyCargoContainer ||
+                                        cube is IMyGasGenerator   ||
+                                        cube is IMyGasTank        ||
+                                        cube is IMyReactor        
+                                        )
                     {
-                        if (!cube.IsFunctional)
-                            EnableBlockHighlightRepair(cube);
+                        if (IsPressed == true )
+                        {
+                            if (!cube.IsFunctional)
+                                EnableBlockHighlightRepair(cube);
+                            else
+                                EnableBlockHighlightHealthy(cube);
+                        }
                         else
-                            EnableBlockHighlightHealthy(cube);
-                    }                    
+                        {
+                            DisableBlockHighlight(cube);
+                        }
+                    }
+                    
                 }
-
-                MyVisualScriptLogicProvider.ButtonPressedEntityName;
-
-
-
-
-                // MyResourceSinkComponentBase sinkComponent;
-                // sinkComponent.SetMaxRequiredInputByType();
 
                 // Whats going on? (Custom, Ingame Breakpoint)
                 // MyAPIGateway.Utilities.ShowMessage("", $"");
             }
         }
+
 
         /// <summary>
         /// Enable highlight if block damaged, set health status color code
@@ -112,13 +116,5 @@ namespace TestEnv
         {
             MyVisualScriptLogicProvider.SetHighlight(block.Name, false, 0, 0, Color.LightGray, -1);
         }
-
-        private void ButtonPressed(string name, int button, long playerID, long blockID)
-        {
-
-        }
-
-
-
     }
 }
---
+++ RadioLucent.cs
@@ -0,0 +1,86 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
+using Sandbox.ModAPI;
+using SpaceEngineers.Game.ModAPI;
+using System.Collections.Generic;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.Entity.UseObject;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace TestEnv
+{
+    /// <summary>
+    /// Radiolucent – Refers to structures that are less dense and permit
+    /// the x-ray beam to pass through them. Radiolucent structures appear
+    /// dark or black in the radiographic image.
+    /// </summary>
+    /// 
+    [MyUseObject("lucent")]
+    class RadioLucent : MyUseObjectBase
+    {
+        public readonly IMyButtonPanel ButtonPanel;
+        public readonly Matrix LocalMatrix;
+        public bool IsPressed { get; set; }
+        public static LucentBlockProjection instance = LucentBlockProjection.Instance;
+        public override UseActionEnum SupportedActions => UseActionEnum.Manipulate
+                                                       | UseActionEnum.UseFinished; // gets called when releasing manipulate
+        
+        public RadioLucent(IMyEntity owner, string dummyName, IMyModelDummy dummyData, uint shapeKey) : base(owner, dummyData)
+        {
+            ButtonPanel = owner as IMyButtonPanel;
+            LocalMatrix = owner.LocalMatrix;
+        }
+
+        public override float InteractiveDistance => 
+            MyConstants.DEFAULT_INTERACTIVE_DISTANCE;
+        public override MatrixD ActivationMatrix => 
+            LocalMatrix * ButtonPanel.WorldMatrix;
+        public override MatrixD WorldMatrix => 
+            ButtonPanel.WorldMatrix;
+        public override UseActionEnum PrimaryAction => 
+            UseActionEnum.Manipulate;
+        public override UseActionEnum SecondaryAction =>
+            UseActionEnum.OpenTerminal;
+
+        public override MyActionDescription GetActionInfo(UseActionEnum actionEnum)
+        {
+            switch (actionEnum)
+            {
+                default:
+                    return default(MyActionDescription);
+            }
+        }
+
+        public override void Use(UseActionEnum actionEnum, IMyEntity user)
+        {
+            // Use for breakpoint;
+            // MyAPIGateway.Utilities.ShowNotification($"Use() action={actionEnum}; user={user}");
+
+            switch (actionEnum)
+            {
+                case UseActionEnum.Manipulate:
+                    if (!IsPressed)
+                    {
+                        MyAPIGateway.Utilities.ShowNotification("Radiolucent Projection Enabled");
+                        instance.IsPressed = true;
+                        IsPressed = !IsPressed;
+                    }
+                    else
+                    {
+                        MyAPIGateway.Utilities.ShowNotification("Radiolucent Projection Disabled");
+                        instance.IsPressed = false;
+                        IsPressed = !IsPressed;                        
+                    }
+                    break;
+            }            
+        }
+    }
+}
